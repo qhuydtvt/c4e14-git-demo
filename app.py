8dbe43e69360aabe592e47ce91bb0eaa8743833e
huynq <qhuydtvt@gmail.com>
2018-01-11 20:17:30 +0700

    Added quote random back-end

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+from random import choice
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    quotes = [
+        'No pain, no gain',
+        'Love yourself. It is important to stay positive because beauty comes from the inside out.',
+        'Being deeply loved by someone gives you strength, while loving someone deeply gives you courage.',
+        'Let us always meet each other with smile, for the smile is the beginning of love.'
+    ]
+    quote_content = choice(quotes)
+    return render_template("index.html", quote_content=quote_content)
 
 if __name__ == '__main__':
   app.run(debug=True)
